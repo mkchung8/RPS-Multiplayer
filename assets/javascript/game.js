f7b59a465d80be7deb31bcb3f427065ed7811222
Miyoung Chung <mkchung.8@gmail.com>
2019-10-02 12:45:58 -0700

    initialized firebase data storage and start screen functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,11 +17,14 @@ $(document).ready(function(){
 
   var database = firebase.database();
 
-  var player1Choice = "Rock";
-  var player2Choice = "Paper"; 
+  var player1Choice = "";
+  var player2Choice = ""; 
   var player1Score = 0; 
   var player2Score = 0; 
-  var roundNumber = 0;
+  var roundNumber = 1;
+  var p1Lock = false; 
+  var p2Lock = false; 
+  var start = 0; 
 
   database.ref().set({
     p1Choice: player1Choice,
@@ -30,7 +33,69 @@ $(document).ready(function(){
     p2Score: player2Score,
     round: roundNumber,
   });
+
+  function startscreen() {
+    $(".lead").text("Welcome to the Battledome. Press Start to Begin.")
+
+    let startButton1 = $('<button type="button" id="player1-start" class="btn btn-dark">Player 1 Start</button>');
+    $("#player1-div").append(startButton1);
+    
+    let startButton2 = $('<button type="button" id="player2-start" class="btn btn-dark">Player 2 Start</button>');
+    $("#player2-div").append(startButton2);
+    
+    $("#player1-start").on("click", function(){
+        $("#player1-start").remove();
+        start++;
+        if (start === 1) {
+        $(".lead").text("Waiting for other player to press start...")
+        }
+        else if (start === 2) {
+            $(".lead").text("BATTLE BEGIN!")
+            battleRound();
+         }
+    });
+    
+    $("#player2-start").on("click", function(){
+        $("#player2-start").remove();
+       start++;
+       if (start === 1) {
+        $(".lead").text("Waiting for other player to press start...")
+        }
+        else if (start === 2) {
+            $(".lead").text("BATTLE BEGIN!")
+            battleRound();
+         }
+    });
+
+
+
+  };
+  
+  startscreen(); 
+
   
 
+  function battleRound() {
+     var p1selectRock = $('<button type="button" data-id="rock" class="btn btn-dark btn-lg">Rock</button>');
+     var p1selectPaper = $('<button type="button" data-id="paper" class="btn btn-dark btn-lg">Paper</button>');
+     var p1selectScissors = $('<button type="button" data-id="scissors" class="btn btn-dark btn-lg">Scissors</button>');
+
+     $("#player1-div").append(p1selectRock).append(p1selectPaper).append(p1selectScissors);
+     
+     
+     var p2selectRock = $('<button type="button" data-id="rock" class="btn btn-dark btn-lg">Rock</button>');
+     var p2selectPaper = $('<button type="button" data-id="paper" class="btn btn-dark btn-lg">Paper</button>');
+     var p2selectScissors = $('<button type="button" data-id="scissors" class="btn btn-dark btn-lg">Scissors</button>');
+     
+    $("#player2-div").append(p2selectRock).append(p2selectPaper).append(p2selectScissors);
+};
+  
+  $("#p1rock").on("click",function(){
+      player1Choice = "Rock";
+      console.log("click")
+      database.ref().set({
+        p1Choice: player1Choice,
+      })
+  })
   
 });
