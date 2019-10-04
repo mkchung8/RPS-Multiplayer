2370492700dd4911be821cd5e6ef8a98361f90a8
Miyoung Chung <mkchung.8@gmail.com>
2019-10-03 23:47:30 -0700

    modified code to prevent game from restarting on refresh

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,74 +18,104 @@ var firebaseConfig = {
   //Declaring Global Variables 
 
   var database = firebase.database();
-
-
-  var playerList = database.ref("/player-list");
-  var player1 = database.ref("/player-list/player1"); 
-  var player2 = database.ref("/player-list/player2"); 
-  var playerChat = database.ref("/chat") 
-  var currentPlayer;
+  var players = database.ref();
+  var p1Database = database.ref("player1");
+  var p2Database = database.ref("player2");
+  var playerTurn = database.ref("turn-track");
+  var playerChat = database.ref("chat"); 
+  var player1; 
+  var player2;
   var p1Name; 
-  var p2Name;
+  var p2Name; 
   var p1Lock = false;
   var p2Lock = false;
-  var p1Snapshot;
-  var p2Snapshot;
+  var p1TurnLock = false; 
+  var p2TurnLock = false;
+  var player1Move; 
+  var player2Move;
   var p1Wins = 0; 
   var p2Wins = 0;
   var p1Losses = 0; 
   var p2Losses =0; 
   var ties = 0;
-  
 
+    
   $("#player1-entry").on("click", function(){
-      p1Name = $("#player1-name").val().trim();
-      p1Lock = true;
-      player1.set({
-          name: p1Name,
-          wins: p1Wins,
-          losses: p1Losses, 
-          ties: ties,
-          lock: p1Lock
-      }); 
+    p1Name = $("#player1-name").val().trim();
+    p1Lock = true;      
 
-      
-  });
+    p1Database.set({
+        name: p1Name,
+        wins: p1Wins,
+        losses: p1Losses, 
+        ties: ties,
+        turnLock: p1TurnLock,
+        playerLock: p1Lock
+        });  
+     });
 
   $("#player2-entry").on("click", function(){
     p2Name = $("#player2-name").val().trim();
-    p2Lock = true;
-    $("#player2-div").empty();
-    $("#player2-div").html(`<h2>Player 2 is ${p2Name}</h2><br><br><h1>READY</h1>`);
-    $("#player2-form").remove();
-    
-    
-    database.ref("/player-list/player2").set({
+    p2Lock = true;  
+    p2Database.set({
         name: p2Name,
         wins: p2Wins,
         losses: p2Losses, 
         ties: ties,
-        lock: p2Lock
+        turnLock: p2TurnLock,
+        playerLock: p2Lock
         }); 
+     });
 
-     if ((p1Lock === true) && (p2Lock===true)){
-            gamePlay();
-        }
+    p1Database.on("value",function(snapshot){  
+        if (snapshot.val() !== null){
+        console.log("p1 data: " + snapshot.val());
+        console.log("p1 name: " + snapshot.val().name);
+        p1Lock = snapshot.val().playerLock;
+         if (p1Lock === true) {
+            $("#player1-form").remove();
+            $("#player1-div").text("Player 1 is READY");
+            $(".lead").text("Waiting for Player 2...");
+            gameStart();
+             };  
+            };
+    }, function(errorObject){
+        console.log("The read failed: " + errorObject.code)
+     });
 
+    p2Database.on("value",function(snapshot){  
+        if (snapshot.val() !== null){
+        console.log("p2 data: " + snapshot.val());
+        console.log("p2 name: " + snapshot.val().name);
+        p2Lock = snapshot.val().playerLock;
+         if (p2Lock === true) {
+            $("#player2-form").remove();
+            $("#player2-div").text("Player 2 is READY");
+            $(".lead").text("Waiting for Player 1...");
+            gameStart();
+         };
+        };
+    }, function(errorObject){
+        console.log("The read failed: " + errorObject.code)
     });
-   
     
+    
+    function gameStart() {
+        if ((p1Lock === true) && (p2Lock === true)){
+            $(".lead").text("READY, SET, BATTLE!")
+            setTimeout(gamePlay, 1000*5)
+        }
+    }
+ 
+    function gamePlay() {
+        console.log("game begin")
+    }
+        
+    
+   
+   
 
- database.ref().on("value", function(snapshot){
-      if 
- }, function(errorObject){
-    console.log("The read failed " + errorObject.code)
- });
 
-
-
-
-
-
-
+    
+    
 });
