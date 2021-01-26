e1458a71d3b30de6968a99ca4d6c4170e40c22e5
Miyoung Chung <mkchung.8@gmail.com>
2021-01-25 20:41:48 -0700

    added player set functions to write into firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,53 +16,70 @@ $(document).ready(function () {
   // Declaring Variables 
   var database = firebase.database();
   var playersRef = database.ref("players");
-  var username = "Guest";
   var playerLock = false;
+  var p1Name = "";
+  var p2Name = "";
 
   // Tracks changes in key which contains player objects.
-  playersRef.on("value", function (snapshot) {
-    // Checking to see if players exist. 
-    let playerOneExists = snapshot.child("1").val();
-    let playerTwoExists = snapshot.child("2").val();
-    console.log(`p1: ${playerOneExists}`);
-    console.log(`p2: ${playerTwoExists}`);
-    if (!playerOneExists && !playerTwoExists) {
-      console.log("no players");
-      var p1StartButton = `<button type="button" id="p1-start-button" class="startButton" >
-                              <h3>Player 1</h3> 
-                              <br>
-                              <h4 class="blink">Press to Start</h4>
-                           </button>`;
-      var p2StartButton = `<button type="button" id="p2-start-button" class="startButton">
-                              <h3>Player 2</h3> 
-                              <br>
-                              <h4 class="blink">Press to Start</h4>
-                           </button>`
-      $("#player1-div").append(p1StartButton);
-      $("#player2-div").append(p2StartButton);
+  playersRef.on("value",
+    // successful callback function
+    function (snapshot) {
+      // Checking to see if players exist. 
+      let playerOneExists = snapshot.child("player1").val();
+      let playerTwoExists = snapshot.child("player2").val();
+      console.log(`p1: ${playerOneExists}`);
+      console.log(`p2: ${playerTwoExists}`);
+      if (!playerOneExists && !playerTwoExists) {
+        console.log("no players");
+        p1Name="Guest"; 
+        p2Name="Guest"; 
+        // var p1StartButton = `<button type="button" id="p1-start-button" class="startButton" >
+        //                       <h3>Player 1</h3> 
+        //                       <br>
+        //                       <h4 class="blink">Press to Start</h4>
+        //                    </button>`;
+        // var p2StartButton = `<button type="button" id="p2-start-button" class="startButton">
+        //                       <h3>Player 2</h3> 
+        //                       <br>
+        //                       <h4 class="blink">Press to Start</h4>
+        //                    </button>`;
+        // // $("#player1-div").append(p1StartButton);
+        // $("#player2-div").append(p2StartButton);
 
-      setTimeout(function () {
-        $("#sub-title").html("<p3>Players Press Start!</p3>");
-      }, 2000);
+        // setTimeout(function () {
+        //   $("#sub-title").html("<p3>Players Press Start!</p3>");
+        //   $("#p1-score").html(`<p2 style="color:red;">Waiting for P1...</p2>`);
+        //   $("#p2-score").html(`<p2 style="color:red;">Waiting for P2...</p2>`);
+        // }, 2000);
 
-      $("#p1-start-button").one("click", function () {
-        console.log("p1 button has been presssed");
+        // $("#p1-start-button").one("click", function () {
+        //   console.log("p1 button has been presssed");
+        //   p1Select();
+        // });
         p1Select();
-      });
-      $("#p2-start-button").one("click", function () {
-        console.log("p2 button has been pressed");
         p2Select();
-      });
-    } else if (playerOneExists && !playerTwoExists) {
-      console.log("waiting for player 2")
-    } else if (!playerOneExists && playerTwoExists) {
-      console.log("waiting for player 1")
-    } else if (playerOneExists && playerTwoExists) {
-      console.log("start play");
-      playerLock = true;
-      gamePlay();
-    };
-  });
+        // $("#p2-start-button").one("click", function () {
+        //   console.log("p2 button has been pressed");
+        //   p2Select();
+        // });
+      } else if (playerOneExists && !playerTwoExists) {
+        console.log("waiting for player 2");
+        // $("#p1-title").html(`<p2>Player 1 Ready</p2>`);
+        // $("#player1-form").remove();
+        p2Select(); 
+
+      } else if (!playerOneExists && playerTwoExists) {
+        console.log("waiting for player 1");
+      } else if (playerOneExists && playerTwoExists) {
+        console.log("start play");
+        playerLock=true; 
+        gamePlay();
+      };
+    },
+    // error callback
+    function (error) {
+      console.error(error);
+    });
 
   function blinkIt() {
     var blinks = document.getElementsByClassName("blink");
@@ -71,49 +88,98 @@ $(document).ready(function () {
       var visiblity = blink.style.visibility;
       blink.style.visibility = visiblity == 'visible' ? 'hidden' : 'visible';
     }
-  }
+  };
   setInterval(blinkIt, 500 /* blinking interval in ms */);
+
+  function capitalize(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  };
 
   function p1Select() {
     console.log('p1 select exec');
-    var p1Input = `<form id="player1-form" class="player-form">
+
+    var p1StartButton = `<button type="button" id="p1-start-button" class="startButton" >
+                           <h3>Player 1</h3> 
+                           <br>
+                           <h4 class="blink">Press to Start</h4>
+                         </button>`;
+    $("#player1-div").append(p1StartButton);
+
+    $("#p1-start-button").one("click", function () {
+      event.preventDefault();
+      $("#p1-start-button").remove();
+      $("#p1-title").text("Player 1");
+      var p1Input = `<form id="player1-form" class="player-form">
                         <p1>Enter Name: </p1><br><input id="player1-name" type="text">
                         <input id="player1-entry" type="submit" value="Submit">
                        </form>`;
-    $("#p1-title").text("Player 1");
-    $("#p1-start-button").remove();
-    $("#player1-div").append(p1Input);
-    $("#player1-entry").one("click", function () {
-      event.preventDefault();
-
-      playersRef.child("player1").set({
-
+      $("#player1-div").append(p1Input);
+      $("#player1-entry").on("click", function () {
+        event.preventDefault();
+        if ($("#player1-name").val() !== "") {
+          p1Name = capitalize($("#player1-name").val());
+          console.log(`p1 name is :${p1Name}`);
+          $("#p1-score").html(`<p2 style="color:red;">P1 Ready!</p2>`);
+          p1Set(p1Name);
+        } else {
+          console.log(`p1 name is: guest`);
+          p1Set(p1Name);
+        }
       });
-      console.log(`player 1 exists as: ${username}`)
+    });
+  };
+
+  function p1Set(p1Name) {
+    console.log("p1 set execute " + p1Name);
+    playersRef.child("player1").set({
+      p1Name: p1Name,
+      wins: 0,
+      losses: 0,
+      choice: null,
+      turnLock: true
     });
   };
 
   function p2Select() {
     console.log('p2 select exec');
-    $("#p2-title").text("Player 2");
-    var p2Input = `<form id="player2-form" class="player-form">
-                        <p1>Player 2 :</p1><br> <input id="player2-name" type="text">
+
+    var p2StartButton = `<button type="button" id="p2-start-button" class="startButton">
+                          <h3>Player 2</h3> 
+                          <br>
+                          <h4 class="blink">Press to Start</h4>
+                        </button>`;
+    $("#player2-div").append(p2StartButton);
+
+    $("#p2-start-button").one("click", function () {
+      event.preventDefault();
+      $("#p2-start-button").remove();
+      $("#p2-title").text("Player 2");
+      var p2Input = `<form id="player2-form" class="player-form">
+                        <p1>Enter Name:</p1><br> <input id="player2-name" type="text">
                         <input id="player2-entry" type="submit" value="Submit">
                        </form>`;
-    $("#player2-div").html(p2Input);
-
-    $("#player2-entry").on("click", function () {
-      event.preventDefault();
-      if ($("#player2-name").val() !== "") {
-        username = capitalize($("#player2-name").val());
-        playersRef.child("player2").set({
-          name: username,
-          wins: 0,
-          losses: 0,
-          choice: null,
-          turnLock: true
-        });
-      };
+      $("#player2-div").append(p2Input);
+      $("#player2-entry").on("click", function(){
+        event.preventDefault(); 
+        if ($("#player2-name").val() !== "") {
+          p2Name = capitalize($("#player2-name").val());
+          console.log(`p2 name is ${p2Name}`); 
+          p2Set(p2Name); 
+        } else {
+          p2Name="Guest";
+          p2Set(p2Name);
+        }
+      });
+    });
+  };
+  function p2Set(p2Name) {
+    console.log("p2 set exec");
+    playersRef.child("player2").set({
+      p2Name: p2Name, 
+      wins: 0,
+      losses: 0, 
+      choice: null, 
+      turnLock: true
     });
   };
 
@@ -123,6 +189,10 @@ $(document).ready(function () {
 
   function resetGame() {
     console.log("reset game exec");
+  };
+
+  function busyGame() {
+    console.log("sorry this game is already full");
   };
 
 });
